80c57647317a49d788b6a7bda97d7c4ab5008bf1
Philippe Brossier <philippe.brossier@ons.gov.uk>
2016-10-12 06:57:16 +0100

    CTPA-298 removing generic imports

--- src/main/java/uk/gov/ons/ctp/sdx/endpoint/PaperReceiptEndpoint.java
+++ src/main/java/uk/gov/ons/ctp/sdx/endpoint/PaperReceiptEndpoint.java
@@ -11,7 +11,11 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 import java.io.InputStream;
 import java.net.URI;
 
