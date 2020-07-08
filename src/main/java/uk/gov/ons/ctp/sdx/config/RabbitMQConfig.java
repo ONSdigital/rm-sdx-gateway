4b130ee6060a54e29e425a263551568ecb8efea4
jterryons <john.terry@ons.gov.uk>
2020-07-08 13:12:32 +0100

    http for dev env

--- src/main/java/uk/gov/ons/ctp/sdx/config/RabbitMQConfig.java
+++ src/main/java/uk/gov/ons/ctp/sdx/config/RabbitMQConfig.java
@@ -9,7 +9,6 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MarshallingMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
